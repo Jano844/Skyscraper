896000e05364ad12d59fac29fa68e120eadf60c5
Jano844 <jani.saenger@gmail.com>
2024-04-29 03:29:07 +0200

    Functions done

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ cells.forEach(cell => {
 	});
 });
 
+let map = [];
+map[0] = [0, 0, 0, 0];
+map[1] = [0, 0, 0, 0];
+map[2] = [0, 0, 0, 0];
+map[3] = [0, 0, 0, 0]
 
 const button1 = document.getElementById('btn1');
 button1.addEventListener('click', () => {
@@ -80,7 +85,7 @@ newPuzzel.addEventListener('click', () => {
 	const p = document.querySelector('.cell:nth-child(34)');
 	const m = document.querySelector('.cell:nth-child(35)');
 
-	randomNumber = getRandomInt(0, 1);
+	randomNumber = getRandomInt(0, 2);
 	currentMap = randomNumber;
 
 	a.textContent = mapArray[randomNumber][0];
@@ -95,16 +100,26 @@ newPuzzel.addEventListener('click', () => {
 	j.textContent = mapArray[randomNumber][9];
 	k.textContent = mapArray[randomNumber][10];
 	l.textContent = mapArray[randomNumber][11];
-	m.textContent = mapArray[randomNumber][12];
-	n.textContent = mapArray[randomNumber][13];
-	o.textContent = mapArray[randomNumber][14];
-	p.textContent = mapArray[randomNumber][15];
+	n.textContent = mapArray[randomNumber][12];
+	o.textContent = mapArray[randomNumber][13];
+	p.textContent = mapArray[randomNumber][14];
+	m.textContent = mapArray[randomNumber][15];
 });
 
 const checkPuzzel = document.getElementById('checkPuzzel');
 checkPuzzel.addEventListener('click', () => {
-	if (checkMap())
+	var correct = document.querySelector('.correct');
+	var incorrect = document.querySelector('.incorrect');
+	correct.style.display = "none";
+	incorrect.style.display = "none";
+
+	if (checkMap() == true) {
+		clearAllCells();
 		newPuzzel.click();
+		correct.style.display = "flex";
+	}
+	else
+		incorrect.style.display = "flex";
 });
 
 function getRandomInt(min, max) {
@@ -118,62 +133,14 @@ function getRandomInt(min, max) {
 // third 4 = right row
 // fourth 4 = bottom row
 const mapArray = [
-	[2, 2, 3 , 1, 2, 2, 3, 1, 1, 2, 3, 2, 1, 2, 2, 3],
-	[1, 3, 4, 2, 1, 3, 2, 2, 2, 1, 3, 2, 2, 2, 1, 2]
+	[2, 2, 1, 3, 3, 1, 2, 3, 2, 2, 3, 1, 3, 2, 2, 1],
+	[4, 1, 2, 2, 2, 3, 2, 1, 3, 2, 1, 2, 1, 4, 2, 2],
+	[1, 2, 2, 3, 1, 2, 4, 2, 3, 2, 1, 3, 2, 1, 3, 2]
 ];
 
-currentMap = 0;
+let currentMap = 0;
 
-// function start() {
-// 	clearAllCells();
-
-// 	const a = document.querySelector('.cell:nth-child(2)');
-// 	const b = document.querySelector('.cell:nth-child(3)');
-// 	const c = document.querySelector('.cell:nth-child(4)');
-// 	const d = document.querySelector('.cell:nth-child(5)');
-
-// 	const e = document.querySelector('.cell:nth-child(7)');
-// 	const f = document.querySelector('.cell:nth-child(13)');
-// 	const g = document.querySelector('.cell:nth-child(19)');
-// 	const h = document.querySelector('.cell:nth-child(25)');
-
-// 	const i = document.querySelector('.cell:nth-child(12)');
-// 	const j = document.querySelector('.cell:nth-child(18)');
-// 	const k = document.querySelector('.cell:nth-child(24)');
-// 	const l = document.querySelector('.cell:nth-child(30)');
-
-// 	const n = document.querySelector('.cell:nth-child(32)');
-// 	const o = document.querySelector('.cell:nth-child(33)');
-// 	const p = document.querySelector('.cell:nth-child(34)');
-// 	const m = document.querySelector('.cell:nth-child(35)');
-
-// 	randomNumber = getRandomInt(0, 1);
-// 	currentMap = randomNumber;
-
-// 	a.textContent = mapArray[randomNumber][0];
-// 	b.textContent = mapArray[randomNumber][1];
-// 	c.textContent = mapArray[randomNumber][2];
-// 	d.textContent = mapArray[randomNumber][3];
-// 	e.textContent = mapArray[randomNumber][4];
-// 	f.textContent = mapArray[randomNumber][5];
-// 	g.textContent = mapArray[randomNumber][6];
-// 	h.textContent = mapArray[randomNumber][7];
-// 	i.textContent = mapArray[randomNumber][8];
-// 	j.textContent = mapArray[randomNumber][9];
-// 	k.textContent = mapArray[randomNumber][10];
-// 	l.textContent = mapArray[randomNumber][11];
-// 	m.textContent = mapArray[randomNumber][12];
-// 	n.textContent = mapArray[randomNumber][13];
-// 	o.textContent = mapArray[randomNumber][14];
-// 	p.textContent = mapArray[randomNumber][15];
-// }
-
-map [
-	[0, 0, 0, 0],
-	[0, 0, 0, 0],
-	[0, 0, 0, 0],
-	[0, 0, 0, 0]
-];
+newPuzzel.click();
 
 function clearMap() {
 	for (let i = 0; i < 4; i++) {
@@ -197,6 +164,7 @@ function getCellContent(cell) {
 }
 
 function getMap() {
+	console.log("getMap\n");
 	map[0][0] = getCellContent(document.querySelector('.cell:nth-child(8)'));
 	map[0][1] = getCellContent(document.querySelector('.cell:nth-child(9)'));
 	map[0][2] = getCellContent(document.querySelector('.cell:nth-child(10)'));
@@ -235,11 +203,11 @@ function checkdoubles() {
 		return false;
 	if (checkdoublesHelper(map[0][0], map[1][0], map[2][0], map[3][0]))
 		return false;
-	if (checkdoublesHelper(map[0][1], map[1][1], map[2][1], map[3][0]))
+	if (checkdoublesHelper(map[0][1], map[1][1], map[2][1], map[3][1]))
 		return false;
-	if (checkdoublesHelper(map[0][2], map[1][0], map[2][2], map[3][0]))
+	if (checkdoublesHelper(map[0][2], map[1][2], map[2][2], map[3][2]))
 		return false;
-	if (checkdoublesHelper(map[0][3], map[1][0], map[2][3], map[3][0]))
+	if (checkdoublesHelper(map[0][3], map[1][3], map[2][3], map[3][3]))
 		return false;
 	return true
 }
@@ -263,29 +231,31 @@ function checkHeightHelper(nb1, nb2, nb3, nb4, first, second) {
 		count++;
 		curHeight = nb4;
 	}
-	if (count != first)
+	if (count != first) {
 		return true;
+	}
 
 	count = 0;
 	curHeight = 0;
 	if (nb4 > curHeight) {
 		count++;
-		curHeight = nb1;
+		curHeight = nb4;
 	}
 	if (nb3 > curHeight) {
 		count++;
-		curHeight = nb2;
+		curHeight = nb3;
 	}
 	if (nb2 > curHeight) {
 		count++;
-		curHeight = nb3;
+		curHeight = nb2;
 	}
 	if (nb1 > curHeight) {
 		count++;
-		curHeight = nb4;
+		curHeight = nb1;
 	}
-	if (count != second)
+	if (count != second) {
 		return true;
+	}
 
 	return false;
 }
@@ -301,11 +271,11 @@ function checkHeight() {
 		return false;
 	if (checkHeightHelper(map[0][0], map[1][0], map[2][0], map[3][0], mapArray[currentMap][0], mapArray[currentMap][12]))
 		return false;
-	if (checkHeightHelper(map[0][1], map[1][1], map[2][1], map[3][0], mapArray[currentMap][1], mapArray[currentMap][13]))
+	if (checkHeightHelper(map[0][1], map[1][1], map[2][1], map[3][1], mapArray[currentMap][1], mapArray[currentMap][13]))
 		return false;
-	if (checkHeightHelper(map[0][2], map[1][0], map[2][2], map[3][0], mapArray[currentMap][2], mapArray[currentMap][14]))
+	if (checkHeightHelper(map[0][2], map[1][2], map[2][2], map[3][2], mapArray[currentMap][2], mapArray[currentMap][14]))
 		return false;
-	if (checkHeightHelper(map[0][3], map[1][0], map[2][3], map[3][0], mapArray[currentMap][3], mapArray[currentMap][15]))
+	if (checkHeightHelper(map[0][3], map[1][3], map[2][3], map[3][3], mapArray[currentMap][3], mapArray[currentMap][15]))
 		return false;
 	return true
 }
@@ -322,14 +292,21 @@ function checkMap() {
 		}
 	}
 	// check if there are no doubles in the rows and columns
-	if (isCorrect)
+	if (isCorrect) {
+		console.log("no 0 left\n");
 		isCorrect = checkdoubles();
-
+	}
 	// check if the map is correct
-	if (isCorrect)
+	if (isCorrect) {
+		console.log("no doubles\n");
 		isCorrect = checkHeight();
+	}
+
+	if (isCorrect)
+		console.log("correct\n");
+	else
+		console.log("incorrect\n");
 	return isCorrect;
 }
 
-newPuzzel.click();
 
